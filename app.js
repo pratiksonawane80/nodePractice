6e4c6c750ce0319deca36932c513cf9cd266ba76
pratiksonawane80 <pratiksonawane80@gmail.com>
2023-08-05 23:38:02 +0530

    assignment 1

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-// CREATE SERVER
-
 const http  = require("http")
 
-// Long way
-// function rqListener(req, res) {
+const routes = require("./routes.js")
 
-// }
-// http.createServer(rqListener)
-
-// Short way
-const server = http.createServer((req, res) => {
-    console.log(req)
-    res.setHeader("Content-Type", "text/html")
-    res.write("<html><head><title>Enter Message</title></head><body><form action='/message' method='POST'><input type='text' type='submit'/><button typr='submit'>Submit</button></form></body></html>")
-    res.end()
-    // for quitting server
-    // process.exit()
-})
+const server = http.createServer(routes.handler)
 
 server.listen("3001")
