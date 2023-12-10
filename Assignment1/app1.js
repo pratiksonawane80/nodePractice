1142bbdffcdfcced78769daba086a80430baa30e
pratiksonawane80 <pratiksonawane80@gmail.com>
2023-12-10 11:43:36 +0530

    Upto node.js

--- Assignment1/app1.js
+++
@@ -1,7 +0,0 @@
-const http = require("http")
-
-const routes = require("./routes1.js")
-
-const server = http.createServer(routes.handler)
-
-server.listen("3002")
--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http  = require("http")
+const path = require('path');
 
-const routes = require("./routes.js")
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes.handler)
+const errorController = require('./controllers/error');
 
-server.listen("3001")
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
